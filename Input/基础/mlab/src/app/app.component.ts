c0a00c95addd86d1a2f81f595f0b20e83da85f83
y1笑而过 <15335747148@163.com>
2018-07-29 17:41:31 +0800

    基础
    基础

---
+++ Input/基础/mlab/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+/**
+ * Input
+ * matInput是一个允许原生<input>和<textarea>元素与<mat-form-field>一起使用的指令。
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+}
