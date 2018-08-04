65064b81bc57d749c9b5e16b7b730951e9fff42e
y1笑而过 <15335747148@163.com>
2018-08-04 16:27:48 +0800

    基础
    基础

---
+++ Toolbar/基础/mlab/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+/**
+ * Toolbar
+ *
+ * <mat-toolbar>是headers，titles或actions的容器。
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
